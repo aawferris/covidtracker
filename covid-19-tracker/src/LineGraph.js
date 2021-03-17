f11d46f3d47ed5ebee808c6c0dd662ff6517aedf
Andrew Ferris <aawferris@gmail.com>
2021-03-17 16:42:09 -0400

    line graph now working

--- covid-19-tracker/src/LineGraph.js
+++ covid-19-tracker/src/LineGraph.js
@@ -1,16 +1,64 @@
 import React, {useState, useEffect} from 'react'
 import { Line } from 'react-chartjs-2'
+import numeral from 'numeral'
+
+const options = {
+    legend: {
+        display: false,
+    },
+    elements: {
+        point: {
+            radius: 0,
+        },
+    },
+    maintainAspectRatio: false,
+    tooltips: {
+        mode: "index",
+        intersect: false,
+        callbacks: {
+            label: function (tooltipItem, data) {
+                return numeral(tooltipItem.value).format("+0,0")
+            },
+        },
+    },
+    scales: {
+        xAxes: [
+            {
+                type: "time",
+                time: {
+                    format: "MM/DD/YY",
+                    tooltipFormat: "ll",
+                },
+            },
+        ],
+        yAxes: [
+            {
+                gridLines: {
+                    display: false,
+                },
+                ticks: {
+                    callback: function (value, index, values) {
+                        return numeral(value).format("0a")
+                    },
+                },
+            },
+        ],
+    }
+}
 
 function LineGraph() {
     const [data, setData] = useState({})
 
    useEffect(() => {
-       fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-       .then(response => response.json())
-       .then((data) => {
-        const chartData = buildChartData(data)
-        setData(chartData)
-       })
+       const fetchData = async () => {
+           await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+           .then(response => {return response.json()})
+           .then((data) => {
+            let chartData = buildChartData(data)
+            setData(chartData)
+           })
+       }
+       fetchData()
    }, [])
 
    const buildChartData = (data, casesType = 'cases') => {
@@ -31,14 +79,18 @@ function LineGraph() {
 
     return (
         <div>
-            <Line data={{
-                datasets: [{
-                    backgroundColor: "rgba(204,16,52,.5)",
-                    borderColor: "#CC1034",
-                    data: data,
-                }]
-            }}
-            />
+            {data?.length > 0 && (
+                <Line 
+                options={options}
+                data={{
+                    datasets: [{
+                        backgroundColor: "rgba(204,16,52,.5)",
+                        borderColor: "#CC1034",
+                        data: data,
+                    }]
+                }}
+                />
+            )}
         </div>
     )
 }
