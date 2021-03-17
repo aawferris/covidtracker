fd0afef9fa821408ef933127a708d238362ceaca
Andrew Ferris <aawferris@gmail.com>
2021-03-17 14:42:51 -0400

    stopped at 2:55:10

--- covid-19-tracker/src/LineGraph.js
+++ covid-19-tracker/src/LineGraph.js
@@ -7,26 +7,38 @@ function LineGraph() {
    useEffect(() => {
        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
        .then(response => response.json())
-       .then(data => {
+       .then((data) => {
+        const chartData = buildChartData(data)
+        setData(chartData)
        })
    }, [])
 
-   const buildChartData = data => {
+   const buildChartData = (data, casesType = 'cases') => {
        const chartData = []
        let lastDataPoint;
-       data.cases.forEach(date => {
+       for (let date in data.cases) {
            if(lastDataPoint) {
                const newDataPoint = {
-                   x: Date,
-                   y: data['cases'][date] - lastDataPoint
+                   x: date,
+                   y: data[casesType][date] - lastDataPoint
                }
-           }
-       })
+               chartData.push(newDataPoint)
+            }
+            lastDataPoint = data[casesType][date]
+       }
+       return chartData
    }
 
     return (
         <div>
-            <h1>I'm a graph.</h1>
+            <Line data={{
+                datasets: [{
+                    backgroundColor: "rgba(204,16,52,.5)",
+                    borderColor: "#CC1034",
+                    data: data,
+                }]
+            }}
+            />
         </div>
     )
 }
